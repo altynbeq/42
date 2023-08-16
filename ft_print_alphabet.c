2ad9b7e139b0c0593b2fda9e127227e0f6b0d923
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-08-16 19:30:05 +0200

    Exercise 01: ft_print_alphabet

---
+++ ft_print_alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_print_alphabet(){
+    
+    char letter = 'a';
+    
+    while(letter <= 'z'){
+        write(1, &letter, 1);
+        letter++;
+        write(1, "\n", 1);
+    }
+}
+
+int main(){
+    ft_print_alphabet();
+    return 0;
+}
