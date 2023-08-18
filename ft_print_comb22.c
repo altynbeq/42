76e3f7bafd4766d1b56e06a36e4339af6aa76b77
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-08-18 12:56:25 +0200

    Update ft_print_comb22.c

--- ft_print_comb22.c
+++ ft_print_comb22.c
@@ -1,50 +1,34 @@
 #include <unistd.h>
 
+void	ft_print_comb2(void)
+{
+	char	digits[5];
+	int	i;
+	int	j;
 
-void display_func(int a, int b, int c, int d){
-    write(1, &a, 1);
-    write(1, &b, 1);
-    write(1, &" ", 1);
-    write(1, &c, 1);
-    write(1, &d, 1);
-        write(1, &", ", 1);
-    
-}
-
-void decider_func(a,b,c,d){
-    if( d == '9' ){
-        c++;
-        d = 0;
-        display_func(a, b, c, d);
-    } else if(c == '9'){
-        c = 0;
-        b++;
-        display_func(a, b, c, d);
-    } else if(b == '9'){
-        b = 0;
-        c++;
-        display_func(a, b, c, d);
-    } 
-}
-
-void ft_print_comb_2(void) {
-    int a;
-    int b;
-    int c;
-    int d;
-    a = '0';
-    b = '0';
-    c = '0';
-    d = '1';
-    while( c <= '9' ){
-        display_func(a, b, c, d);
-        d++;
-    }
-    decider_func(a,b,c,d);
-    
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			digits[0] = '0' + i / 10;
+			digits[1] = '0' + i % 10;
+			digits[2] = ' ';
+			digits[3] = '0' + j / 10;
+			digits[4] = '0' + j % 10;
+			write (1, digits, 5);
+			if (i != 98 || j != 99)
+			{
+				write(1, ", ", 2);
+			}
+			j++;
+		}
+		i++;
+	}
 }
 
 int main(){
-    ft_print_comb_2();
+    ft_print_comb2();
     return 0;
 }
