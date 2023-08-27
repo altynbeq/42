7704c8a62cdd65425e4bb4a095fe0c1e8c7791e8
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-08-27 15:50:29 +0200

    Update ex03.c
    too long, but it works! :)

--- c04/ex03.c
+++ c04/ex03.c
@@ -1,4 +1,3 @@
-// not finished solution
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -7,74 +6,91 @@
 /*   By: akuat <akuat@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 10:47:14 by akuat             #+#    #+#             */
-/*   Updated: 2023/08/27 14:13:56 by akuat            ###   ########.fr       */
+/*   Updated: 2023/08/27 15:47:01 by akuat            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-// void	ft_putchar(char c, int neg_check)
-// {
-// 	if (neg_check != 0)
-// 		write(1, "-", 1);
-// 	write(1, &c, 1);
-// }
+int	find_first(char *str)
+{
+	int	i;
 
-int    find_first(char *str){
-    int i = 0;
-    while(str[i] != '\0'){
-        if(str[i] >= '0' && str[i] <= '9')
-            break;
-        i++;
-    }
-    return(i);
-}
-
-int    find_last(char *str, int i){
-    while(str[i] != '\0'){
-	    if( !(str[i] >= '0' && str[i] <= '9') ){
-	        break;
-	    }
-	    i++;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= '0' && str[i] <= '9')
+			break ;
+		i++;
 	}
-	return(i);
+	return (i);
 }
 
-int neg_check(char *str, int j){
-    int i;
-    int counter;
-    
-    i = 0;
-    counter = 0;
-    while(i != j){
-        if(str[i] == '-'){
-          counter++;  
-        }
-        i++;
-    }
-    return(counter);
+int	find_last(char *str, int i)
+{
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= '0' && str[i] <= '9'))
+			break ;
+		i++;
+	}
+	return (i);
+}
+
+int	neg_check(char *str, int j)
+{
+	int	i;
+	int	counter;
+
+	i = 0;
+	counter = 0;
+	while (i != j)
+	{
+		if (str[i] == '-')
+			counter++;
+		i++;
+	}
+	return (counter);
+}
+
+int	calc_total(char *str_ref)
+{
+	int	total;
+	int	i;
+
+	i = 0;
+	total = 0;
+	while (str_ref[i] != '\0')
+	{
+		total = total * 10 + (str_ref[i] - '0');
+		i++;
+	}
+	return (total);
 }
 
 int	ft_atoi(char *str)
 {
-	int	first_i;
-	int	last_i;
-	char    *str_ref;
-	int neg;
-	
-	str_ref = str;
+	int		first_i;
+	int		last_i;
+	char	str_ref[100];
+	int		neg;
+	int		i;
+
+	i = 0;
 	first_i = find_first(str);
 	last_i = find_last(str, first_i);
 	neg = neg_check(str, first_i);
-	
-	if(neg%2 != 0)
-	    write(1, "-", 1);
-	while(first_i != last_i){
-	    char ans = str[first_i] + '0';
-	    write(1, &str[first_i], 1);
-	    first_i++;
+	if (neg % 2 != 0)
+		write(1, "-", 1);
+	while (first_i < last_i)
+	{
+		str_ref[i] = str[first_i];
+		first_i++;
+		i++;
 	}
-	return (0);
+	str_ref[i] = '\0';
+	neg = calc_total(str_ref);
+	return (neg);
 }
 
 int	main(void)
