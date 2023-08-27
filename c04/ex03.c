d184b5a896d3409ebc2466ee823c74e3380a8a77
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-08-27 14:44:20 +0200

    Update ex03.c

--- c04/ex03.c
+++ c04/ex03.c
@@ -20,31 +20,60 @@
 // 	write(1, &c, 1);
 // }
 
-void	index_finder(char *str, int **first_i, int **last_i, int **neg)
-{
+int    find_first(char *str){
+    int i = 0;
+    while(str[i] != '\0'){
+        if(str[i] >= '0' && str[i] <= '9')
+            break;
+        i++;
+    }
+    return(i);
+}
+
+int    find_last(char *str, int i){
+    while(str[i] != '\0'){
+	    if( !(str[i] >= '0' && str[i] <= '9') ){
+	        break;
+	    }
+	    i++;
+	}
+	return(i);
+}
+
+int neg_check(char *str, int j){
+    int i;
+    int counter;
+    
+    i = 0;
+    counter = 0;
+    while(i != j){
+        if(str[i] == '-'){
+          counter++;  
+        }
+        i++;
+    }
+    return(counter);
 }
 
 int	ft_atoi(char *str)
 {
 	int	first_i;
 	int	last_i;
-	int	neg;
-	int	i;
-
-	i = 0;
-	first_i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= '0' && str[i] <= '9')
-			first_i = i;
-		if (str[i] == '-')
-			neg++;
-		if (str[i] >= '0' && str[i] <= '9')
-			last_i = i;
-		i++;
+	char    *str_ref;
+	int neg;
+	
+	str_ref = str;
+	first_i = find_first(str);
+	last_i = find_last(str, first_i);
+	neg = neg_check(str, first_i);
+	
+	if(neg%2 != 0)
+	    write(1, "-", 1);
+	while(first_i != last_i){
+	    char ans = str[first_i] + '0';
+	    write(1, &str[first_i], 1);
+	    first_i++;
 	}
-	char ans = first_i + '0';
-	write (1, &ans, 1);
 	return (0);
 }
 
@@ -52,7 +81,7 @@ int	main(void)
 {
 	char	*str;
 
-	str = "-++12c4a";
+	str = "---+1c2c4a";
 	ft_atoi(str);
 	return (0);
 }
