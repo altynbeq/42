9941482cb7b46d2eba846f05ffdbeb5783f7d561
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-08-28 11:40:34 +0200

    Update ex03.c

--- c04/ex03.c
+++ c04/ex03.c
@@ -6,98 +6,54 @@
 /*   By: akuat <akuat@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 10:47:14 by akuat             #+#    #+#             */
-/*   Updated: 2023/08/27 15:47:01 by akuat            ###   ########.fr       */
+/*   Updated: 2023/08/28 11:39:53 by akuat            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-int	find_first(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= '0' && str[i] <= '9')
-			break ;
-		i++;
-	}
-	return (i);
-}
-
-int	find_last(char *str, int i)
-{
-	while (str[i] != '\0')
-	{
-		if (!(str[i] >= '0' && str[i] <= '9'))
-			break ;
-		i++;
-	}
-	return (i);
-}
-
-int	neg_check(char *str, int j)
-{
-	int	i;
-	int	counter;
-
-	i = 0;
-	counter = 0;
-	while (i != j)
-	{
-		if (str[i] == '-')
-			counter++;
-		i++;
-	}
-	return (counter);
-}
-
-int	calc_total(char *str_ref)
-{
-	int	total;
-	int	i;
-
-	i = 0;
-	total = 0;
-	while (str_ref[i] != '\0')
-	{
-		total = total * 10 + (str_ref[i] - '0');
-		i++;
-	}
-	return (total);
-}
+#include <stdio.h>
 
 int	ft_atoi(char *str)
 {
-	int		first_i;
-	int		last_i;
-	char	str_ref[100];
-	int		neg;
-	int		i;
+	int	num;
+	int	sign;
 
-	i = 0;
-	first_i = find_first(str);
-	last_i = find_last(str, first_i);
-	neg = neg_check(str, first_i);
-	if (neg % 2 != 0)
-		write(1, "-", 1);
-	while (first_i < last_i)
+	sign = 0;
+	num = 0;
+	while ((*str == ' ' || *str == '\t' || *str == '\n' || 
+			*str == '\f' || *str == '\v' || *str == '\r'))
+		str++;
+	while (*str == '-' || *str == '+')
 	{
-		str_ref[i] = str[first_i];
-		first_i++;
-		i++;
+		if (*str == '-')
+			sign--;
+		else if (*str == '+')
+			sign++;
+		str++;
 	}
-	str_ref[i] = '\0';
-	neg = calc_total(str_ref);
-	return (neg);
+	while (*str >= '0' && *str <= '9')
+	{
+		num = num * 10 + (*str - '0');
+		str++;
+	}
+	if (sign < 0)
+		num = -num;
+	return (num);
 }
 
-int	main(void)
+int main()
 {
-	char	*str;
-
-	str = "---+1c2c4a";
-	ft_atoi(str);
-	return (0);
+    char test1[] = " ---+--+1234ab567";
+    char test2[] = "   +42";
+    char test3[] = "   -9999abc";
+    char test4[] = "12345";
+    char test5[] = "   ";
+    
+    printf("atoi(%s) = %d\n", test1, ft_atoi(test1));
+    printf("atoi(%s) = %d\n", test2, ft_atoi(test2));
+    printf("atoi(%s) = %d\n", test3, ft_atoi(test3));
+    printf("atoi(%s) = %d\n", test4, ft_atoi(test4));
+    printf("atoi(%s) = %d\n", test5, ft_atoi(test5));
+    
+    return 0;
 }
