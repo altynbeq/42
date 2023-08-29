f20c47989f229a21c393778e655534b9827807bf
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-08-29 13:59:24 +0200

    Create ex06.c

---
+++ c05/ex06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int ft_is_prime(int nb)
+{
+    if(nb < 2)
+        return 0;
+    if( (nb > 1 && nb < 4) || (nb % 2 != 0 && nb % 3 != 0))
+        return 1;
+    else
+        return 0;
+}
+int main() {
+    int nb = 0;
+    int power = 2;
+    int result1 = ft_is_prime(nb);
+    
+    printf("Result: %d\n", result1); 
+    return 0;
+}
