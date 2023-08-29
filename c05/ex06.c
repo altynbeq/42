d106fbcb3f7f7bf47796d0b6c19ee35252b084bb
Altynbek Quat <65371350+altynbeq@users.noreply.github.com>
2023-08-29 14:07:48 +0200

    Update ex06.c

--- c05/ex06.c
+++ c05/ex06.c
@@ -5,7 +5,7 @@ int ft_is_prime(int nb)
 {
     if(nb < 2)
         return 0;
-    if( (nb > 1 && nb < 4) || (nb % 2 != 0 && nb % 3 != 0))
+    if( (nb > 1 && nb < 4) || (nb % 2 != 0 && nb % 3 != 0 && nb % 5 != 0))
         return 1;
     else
         return 0;
